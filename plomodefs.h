14ba4efe2fa89473117d44f35ee69e140099c2d5
silverio <wsilverio@example.com>
2018-10-06 21:10:08 -0300

    assert_param

--- plomodefs.h
+++ plomodefs.h
@@ -74,6 +74,30 @@
 */
 #define MIN(x, y) ( (x)>(y) ? (y) : (x) )
 
+/**
+*  \def GET_ARRAY_LENGTH(arr)
+*   @brief      Determina a quantidade de elementos de um array.
+*   @param      arr array a ser consultado.
+*   @return     quantidade de elementos no array.
+*/
+#define GET_ARRAY_LENGTH(arr) (sizeof(arr)/sizeof((arr[0])))
+
+#ifndef NDEBUG
+#if defined(USE_FULL_ASSERT)
+/**
+*   @brief      Callback da macro \a assert_param, em caso de erro.
+*   @note       Deve ser implementada pelo usuário para tratamento do erro.
+*/
+void assert_failed();
+/**
+*  \def assert_param(expr)
+*   @brief      Testa a expressão é verdadeira. Se for falsa, chama a função de callback.
+*   @param      Expressão a ser testada.
+*/
+#define assert_param(expr) ((expr) ? (void)0 : assert_failed());
+#endif // USE_FULL_ASSERT
+#endif // NDEBUG
+
 /////////////////////////////////////////////////////////////////
 // protótipo das funções
 /////////////////////////////////////////////////////////////////
